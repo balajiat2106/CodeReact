588d501cb02f9271588abdb65625566cd60740dd
Balaji S Prabakaran <balaji.prabakaran87@gmail.com>
2020-05-01 23:32:24 +0530

    form select change event

--- learnforms/src/components/BasicForm.js
+++ learnforms/src/components/BasicForm.js
@@ -11,41 +11,54 @@ export class BasicForm extends Component {
     };
   }
 
+  commentChange = (event) => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+
   nameChange = (event) => {
     this.setState({
       name: event.target.value,
     });
   };
 
-  ClickHandler = event => {
+changeChoice=event=>{
+    this.setState({
+        choice:event.target.value
+    })
+}
+
+  ClickHandler = (event) => {
     this.setState({
       name: this.state.name,
       choice: this.state.choice,
       coments: this.state.comments,
     });
 
-    alert(this.state.name);
+    alert(`${this.state.name} and ${this.state.choice} and ${this.state.comments}`);
     event.preventDefault();
   };
 
   render() {
+    const { name, comments, choice } = this.state;
     return (
       <div>
         <form onSubmit={this.ClickHandler}>
           <div>
             <label>Name: </label>
-            <input type="text" onChange={this.nameChange}></input>
-          </div>
-          <div>
-            <label>Choice: </label>
-            <textarea type="text"></textarea>
+            <input type="text" value={name} onChange={this.nameChange}></input>
           </div>
           <div>
             <label>Comments: </label>
-            <select>
-              <option value="1">React</option>
-              <option value="2">Angular</option>
-              <option value="3">Vue</option>
+            <textarea type="text" value={comments} onChange={this.commentChange}></textarea>
+          </div>
+          <div>
+            <label>Choice: </label>
+            <select value={choice} onChange={this.changeChoice}>
+              <option value="React">React</option>
+              <option value="Angular">Angular</option>
+              <option value="Vue">Vue</option>
             </select>
           </div>
           <button type="submit">Submit</button>
