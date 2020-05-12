3f24a8b7eec66cf4a24e9652d0d01dc52d64d091
Balaji S Prabakaran <balaji.prabakaran87@gmail.com>
2020-05-12 23:52:33 +0530

    Error boundary more

--- learnpuracomponents/src/App.js
+++ learnpuracomponents/src/App.js
@@ -5,6 +5,7 @@ import ClasswithRef from "./components/ClasswithRef";
 import ParentRef from "./components/ParentRef";
 import PortalDemo from "./components/PortalDemo";
 import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
@@ -14,9 +15,15 @@ function App() {
       {/* <ClasswithRef/> */}
       {/* <ParentRef/> */}
       {/* <PortalDemo/> */}
-      <Hero heroName="Balaji" />
-      <Hero heroName="Sanjeev" />
-      <Hero heroName="Joker" />
+      <ErrorBoundary>
+        <Hero heroName="Balaji" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Sanjeev" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
     </div>
   );
 }
