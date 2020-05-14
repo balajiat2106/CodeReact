5453e7e749201dd88febd825f6b63fc573955d26
Balaji S Prabakaran <balaji.prabakaran87@gmail.com>
2020-05-14 23:46:48 +0530

    hoc base preparation

---
+++ hoc/src/Components/SecondChild.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class SecondChild extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  mouseOver = () => [
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
+    }),
+  ];
+  render() {
+      const {count}=this.state
+    return (
+      <div>
+        <h2 onMouseOver={this.mouseOver}>Hovered {count} times</h2>
+      </div>
+    );
+  }
+}
+
+export default SecondChild;
